ff9b3bdade52f8abefe76c443d49132c198e84a3
Gret <gretccx@gmail.com>
2018-11-05 23:43:19 -0600

    Finished styling the large-hero block.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+
+gulp.task('default', function () {
+  console.log("Hooray - you created a Gulp task.")
+});
+
+gulp.task('html', () => {
+  console.log('Imagine something useful being done to your HTML here.');
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () => {
+  watch('./app/index.html', () => {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
+  });
+});
