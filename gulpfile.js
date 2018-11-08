3d00eb689ae8959b838d9a70c29ec7ec2122e5f0
Gret <gretccx@gmail.com>
2018-11-08 14:45:41 -0600

    Organized gulp tasks and added error handling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,4 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssvars = require('postcss-simple-vars');
-const nested = require('postcss-nested');
-const cssImport = require('postcss-import');
-const browserSync = require('browser-sync').create();
+/* dummy code at the start of learning
 
 gulp.task('default', function () {
   console.log("Hooray - you created a Gulp task.")
@@ -15,31 +8,7 @@ gulp.task('html', () => {
   console.log('Imagine something useful being done to your HTML here.');
 });
 
-gulp.task('styles', () => {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
+*/
 
-gulp.task('watch', () => {
-
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: 'app'
-    }
-  });
-
-  watch('./app/index.html', () => {
-    browserSync.reload();
-  });
-
-  watch('./app/assets/styles/**/*.css', () => {
-    gulp.start('cssInject');
-  });
-});
-
-gulp.task('cssInject', ['styles'], () => {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
