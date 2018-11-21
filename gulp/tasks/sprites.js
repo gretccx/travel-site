e9f49952038885af055bc6857ca43bc96d757107
Gret <gretccx@gmail.com>
2018-11-21 08:48:07 -0600

    Completed gulp automation.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 const config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -18,3 +20,16 @@ gulp.task('createSprite', () => {
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], () => {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], () => {
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
